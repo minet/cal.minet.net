8a96a42f35a087e388b90304de27c321bdb0ad65
Alexis Rossfelder <alexis@liteapp.fr>
2026-02-01 23:40:44 +0100

    fix: connection to db drops after extended periods of time causing logouts/auth errors

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,7 @@ from sqlmodel import SQLModel, Session, create_engine
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/calendint")
 
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
 
 def get_session():
     with Session(engine) as session:
